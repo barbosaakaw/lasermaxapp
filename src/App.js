50b3144bf9dddfd2491a7f7fe39b913b680568da
caioalbert <caioalberto2104@gmail.com>
2023-10-18 22:01:44 -0300

    update app

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   };
 
   return (
-    <Router basename="/barbosaakaw/lasermaxapp"> {/* Defina o basename como "/lasermaxapp" */}
+    <Router basename="/lasermaxapp/barbosaakaw"> {/* Defina o basename como "/lasermaxapp" */}
       <div className="App">
         <Routes>
           <Route
